90dda0d3223cdc296ff64a3e0c15aa539fa63159
Naeun Kim <102296721+Nico1eKim@users.noreply.github.com>
2024-11-13 03:37:18 +0900

    fix: 데이터 오류 수정

--- app/src/main/java/com/example/kuit4_android_retrofit/adapter/RVPopularMenuAdapter.kt
+++ app/src/main/java/com/example/kuit4_android_retrofit/adapter/RVPopularMenuAdapter.kt
@@ -2,18 +2,19 @@
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.example.kuit4_android_retrofit.data.MenuCategoryData
-import com.example.kuit4_android_retrofit.databinding.ItemMenuCategoryBinding
+import com.example.kuit4_android_retrofit.R
+import com.example.kuit4_android_retrofit.data.CategoryData
+import com.example.kuit4_android_retrofit.databinding.ItemCategoryBinding
 
 class RVPopularMenuAdapter(
-    private val menuList: List<MenuCategoryData>,
+    private val menuList: List<CategoryData>,
 ) : RecyclerView.Adapter<RVPopularMenuAdapter.ViewHolder>() {
     inner class ViewHolder(
-        private val binding: ItemMenuCategoryBinding,
+        private val binding: ItemCategoryBinding,
     ) : RecyclerView.ViewHolder(binding.root) {
-        fun bind(item: MenuCategoryData) {
-            binding.sivMenuCategoryImg.setImageResource(item.menuCategoryImg)
-            binding.tvMenuCategoryName.text = item.menuCategoryName
+        fun bind(item: CategoryData) {
+            binding.sivCategoryImg.setImageResource(R.drawable.img_bossam)
+            binding.tvCategoryName.text = item.categoryName
         }
     }
 
@@ -22,7 +23,7 @@ class RVPopularMenuAdapter(
         viewType: Int,
     ): ViewHolder {
         val binding =
-            ItemMenuCategoryBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+            ItemCategoryBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return ViewHolder(binding)
     }
 
---
+++ app/src/main/java/com/example/kuit4_android_retrofit/data/CategoryData.kt
@@ -0,0 +1,9 @@
+package com.example.kuit4_android_retrofit.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+data class CategoryData(
+    val categoryName: String,
+    val categoryImg: String,
+)
